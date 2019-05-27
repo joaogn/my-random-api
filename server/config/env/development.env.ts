52e81de560949c95bb96b9d7e0c75dea095ecf0a
mucilon <joao.g.n91@gmail.com>
2019-05-27 20:08:10 -0300

    chage db name

--- server/config/env/development.env.ts
+++ server/config/env/development.env.ts
@@ -1,12 +1,12 @@
 module.exports = {
-    env: 'development',
-    db: 'ts-api',
+    env: 'develop',
+    db: 'my-random-db',
     dialect: 'postgres',
     username: 'postgres',
     password: 'pgroot',
     host: 'localhost',
     serverPort: 3000,
     pgPort: 5432,
-    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api',
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/my-random-db-',
     secret: 'S3cr3t'
 }
